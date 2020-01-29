e002ebb4ec5c0bd48a7b2500342201ee9c03a8b2
caniggia1 <caniggia1@hotmail.com>
2020-01-29 10:42:51 +0100

    Possible to get IP from DHCP

--- Network.h
+++ Network.h
@@ -1,5 +1,6 @@
 // Adjust the following values to match your needs
 // -----------------------------------------------
+//#define USE_DHCP
 #define   servername "fileserver"     // Set your server's logical name here e.g. if myserver then address is http://myserver.local/
 IPAddress local_IP(192, 168, 0, 150); // Set your server's fixed IP address here
 IPAddress gateway(192, 168, 0, 1);    // Set your network Gateway usually your Router base address
